21fea4848bec4a18538899b2e1eee63b961e6e40
Adam Smith <hagansmith@gmail.com>
2017-12-27 20:52:20 -0600

    update add calls

--- server.js
+++ server.js
@@ -13,11 +13,31 @@ app.use(function(req, res, next) {
   next();
 });
 
-// https://safe-mesa-30633.herokuapp.com/api/geocode/?
+// https://safe-mesa-30633.herokuapp.com/api/directions/?
 app.get('/api/directions/*', (req, res) => {
   let apiCall = req.url.slice('/api/directions/'.length)
   console.log(apiCall);
   let apiReq = `https://maps.googleapis.com/maps/api/directions/json${apiCall}`
+  request.get(apiReq, (err, _, body) => {
+    res.send(body)
+  });
+});
+
+// https://safe-mesa-30633.herokuapp.com/api/geocode/?
+app.get('/api/geocode/*', (req, res) => {
+  let apiCall = req.url.slice('/api/geocode/'.length)
+  console.log(apiCall);
+  let apiReq = `https://maps.googleapis.com/maps/api/geocode/json${apiCall}`
+  request.get(apiReq, (err, _, body) => {
+    res.send(body)
+  });
+});
+
+// https://safe-mesa-30633.herokuapp.com/api/place/details/?
+app.get('/api/place/details/*', (req, res) => {
+  let apiCall = req.url.slice('/api/place/details/'.length)
+  console.log(apiCall);
+  let apiReq = `https://maps.googleapis.com/maps/api/place/details/json${apiCall}`
   request.get(apiReq, (err, _, body) => {
     res.send(body)
   });
